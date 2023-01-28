bf7f3c62ef49486fcda63c154c8bd2557a707562
Tiago de Oliveira <74031973+deolivtiago@users.noreply.github.com>
2023-01-28 18:24:30 -0300

    feat: add routes to handle users (#21)
    * refactor: add success flag to the response
    
    * test: update view test to validate success flag
    
    * feat: add changeset response view
    
    * feat: add user response view
    
    * test: ensure changeset view renders changeset errors
    
    * test: ensure user view renders user data
    
    * feat: add fallback controller to handle errors
    
    * feat: add user controller to handle users
    
    * feat: add routes to handle users
    
    * test: ensure user controller handles users

--- lib/api_web/router.ex
+++ lib/api_web/router.ex
@@ -7,6 +7,8 @@ defmodule ApiWeb.Router do
 
   scope "/api", ApiWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Enables LiveDashboard only for development
