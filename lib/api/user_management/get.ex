ba7aa847092314fb4168e3fe72070142445669eb
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-02-03 17:34:26 -0300

    feat: add user recovery by email

--- lib/api/user_management/get.ex
+++ lib/api/user_management/get.ex
@@ -4,6 +4,18 @@ defmodule Api.UserManagement.Get do
   alias Api.UserManagement.User
 
   @doc false
+  def call(email: email) do
+    User
+    |> Repo.get_by!(email: String.downcase(email))
+    |> then(&{:ok, &1})
+  rescue
+    _ ->
+      :email
+      |> User.invalid_changeset(email, "not found")
+      |> then(&{:error, &1})
+  end
+
+  @doc false
   def call(id) do
     User
     |> Repo.get!(id)
