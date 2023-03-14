ecf644e45dcad6b6b310a57970afe14eef6c1d6d
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-03-13 22:07:30 -0300

    feat: add country creation

---
+++ lib/api/location/create.ex
@@ -0,0 +1,12 @@
+defmodule Api.Location.Create do
+  @moduledoc false
+  alias Api.Location.Country
+  alias Api.Repo
+
+  @doc false
+  def call(attrs \\ %{}) do
+    %Country{}
+    |> Country.changeset(attrs)
+    |> Repo.insert()
+  end
+end
