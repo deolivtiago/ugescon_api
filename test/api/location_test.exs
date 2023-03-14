d281ba1a5645a2e95818b6d80b4d0bd2ca55b3e6
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-03-14 00:25:33 -0300

    test: ensure context handles states

--- test/api/location_test.exs
+++ test/api/location_test.exs
@@ -5,12 +5,14 @@ defmodule Api.LocationTest do
 
   alias Api.Location
   alias Api.Location.Country
+  alias Api.Location.State
   alias Ecto.Changeset
 
   setup do
-    attrs = params_for(:country)
+    country_attrs = params_for(:country)
+    state_attrs = params_for(:state)
 
-    {:ok, attrs: attrs}
+    {:ok, country_attrs: country_attrs, state_attrs: state_attrs}
   end
 
   describe "list_countries/0" do
@@ -46,7 +48,7 @@ defmodule Api.LocationTest do
   end
 
   describe "create_country/1 returns :ok" do
-    test "when the country attributes are valid", %{attrs: attrs} do
+    test "when the country attributes are valid", %{country_attrs: attrs} do
       assert {:ok, %Country{} = country} = Location.create_country(attrs)
 
       assert attrs.name == country.name
@@ -75,7 +77,7 @@ defmodule Api.LocationTest do
       assert errors.code == ["can't be blank"]
     end
 
-    test "when the country code already exists", %{attrs: attrs} do
+    test "when the country code already exists", %{country_attrs: attrs} do
       attrs =
         insert(:country)
         |> then(&Map.put(attrs, :code, &1.code))
@@ -91,7 +93,7 @@ defmodule Api.LocationTest do
   describe "update_country/2 returns :ok" do
     setup [:insert_country]
 
-    test "when the country attributes are valid", %{country: country, attrs: attrs} do
+    test "when the country attributes are valid", %{country: country, country_attrs: attrs} do
       assert {:ok, %Country{} = country} = Location.update_country(country, attrs)
 
       assert attrs.name == country.name
@@ -137,9 +139,127 @@ defmodule Api.LocationTest do
     end
   end
 
+  describe "list_states/0" do
+    test "without states returns an empty list" do
+      assert [] == Location.list_states()
+    end
+
+    test "with states returns all states" do
+      state = insert(:state)
+
+      assert [state] == Location.list_states()
+    end
+  end
+
+  describe "get_state/1 returns :ok" do
+    setup [:insert_state]
+
+    test "when the given id is found", %{state: %{id: id} = state} do
+      assert {:ok, %State{} = ^state} = Location.get_state(id)
+    end
+  end
+
+  describe "get_state/1 returns :error" do
+    test "when the given id is not found" do
+      id = Ecto.UUID.generate()
+
+      assert {:error, changeset} = Location.get_state(id)
+      errors = errors_on(changeset)
+
+      refute changeset.valid?
+      assert errors.id == ["not found"]
+    end
+  end
+
+  describe "create_state/1 returns :ok" do
+    test "when the state attributes are valid", %{state_attrs: attrs} do
+      assert {:ok, %State{} = state} = Location.create_state(attrs)
+
+      assert attrs.name == state.name
+      assert attrs.code == state.code
+    end
+  end
+
+  describe "create_state/1 returns :error" do
+    test "when the state attributes are invalid" do
+      attrs = %{code: "??", name: "?"}
+
+      assert {:error, changeset} = Location.create_state(attrs)
+      errors = errors_on(changeset)
+
+      refute changeset.valid?
+      assert errors.name == ["should be at least 2 character(s)"]
+      assert errors.code == ["must contain only characters A-Z"]
+    end
+
+    test "when the state attributes is not provided" do
+      assert {:error, changeset} = Location.create_state()
+      errors = errors_on(changeset)
+
+      refute changeset.valid?
+      assert errors.name == ["can't be blank"]
+      assert errors.code == ["can't be blank"]
+    end
+  end
+
+  describe "update_state/2 returns :ok" do
+    setup [:insert_state]
+
+    test "when the state attributes are valid", %{state: state, state_attrs: attrs} do
+      assert {:ok, %State{} = state} = Location.update_state(state, attrs)
+
+      assert attrs.name == state.name
+      assert attrs.code == state.code
+    end
+  end
+
+  describe "update_state/2 returns :error" do
+    setup [:insert_state]
+
+    test "when the state attributes are invalid", %{state: state} do
+      invalid_attrs = %{code: nil, name: "a"}
+
+      assert {:error, changeset} = Location.update_state(state, invalid_attrs)
+      errors = errors_on(changeset)
+
+      refute changeset.valid?
+      assert ["should be at least 2 character(s)"] == errors.name
+      assert ["can't be blank"] == errors.code
+      assert {:ok, state} == Location.get_state(state.id)
+    end
+  end
+
+  describe "delete_state/1" do
+    setup [:insert_state]
+
+    test "deletes the state", %{state: state} do
+      assert {:ok, %State{}} = Location.delete_state(state)
+
+      assert {:error, changeset} = Location.get_state(state.id)
+      errors = errors_on(changeset)
+
+      refute changeset.valid?
+      assert errors.id == ["not found"]
+    end
+  end
+
+  describe "change_state/1" do
+    setup [:insert_state]
+
+    test "returns a changeset", %{state: state} do
+      assert %Changeset{} = Location.change_state(state)
+    end
+  end
+
   defp insert_country(_) do
     :country
     |> insert()
     |> then(&{:ok, country: &1})
+  end
+
+  defp insert_state(_) do
+    :state
+    |> insert()
+    |> then(&{:ok, state: &1})
   end
 end
