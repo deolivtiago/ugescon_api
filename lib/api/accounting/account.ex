e70c9c01ceeae9b926e9ee5ed8a6825eedede2c4
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-03-21 21:50:15 -0300

    feat: add account validation schema

---
+++ lib/api/accounting/account.ex
@@ -0,0 +1,27 @@
+defmodule Api.Accounting.Account do
+  use Api.Schema
+
+  import Ecto.Changeset
+
+  schema "accounts" do
+    field :level, :integer
+    field :code, :string
+    field :name, :string
+    field :type, Ecto.Enum, values: [debit: 0, credit: 1]
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:code, :name, :type, :level])
+    |> validate_required([:code, :name, :type, :level])
+    |> unique_constraint(:id, name: :accounts_pkey)
+    |> unique_constraint(:code)
+    |> validate_length(:name, min: 2, max: 150)
+    |> validate_length(:code, min: 1, max: 5)
+    |> validate_format(:code, ~r/^[0-9]+$/, message: "must contain only numbers")
+    |> validate_number(:level, greater_than: 0, less_than: 5)
+  end
+end
