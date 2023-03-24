bc239dafd913c58ed876d4775ed7383bc1794be0
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-03-23 22:36:28 -0300

    feat: add migration to create entries table

---
+++ priv/repo/migrations/20230324005035_create_entries.exs
@@ -0,0 +1,27 @@
+defmodule Api.Repo.Migrations.CreateEntries do
+  use Ecto.Migration
+
+  def change do
+    create table(:entries) do
+      add :type, :integer, null: false
+      add :value, :integer, null: false
+      add :description, :string
+
+      add :person_id, references(:persons, on_delete: :delete_all), null: false
+
+      add :debit_account_code,
+          references(:accounts, type: :string, column: :code, on_delete: :restrict),
+          null: false
+
+      add :credit_account_code,
+          references(:accounts, type: :string, column: :code, on_delete: :restrict),
+          null: false
+
+      timestamps()
+    end
+
+    create index(:entries, [:debit_account_code])
+    create index(:entries, [:credit_account_code])
+    create index(:entries, [:person_id])
+  end
+end
