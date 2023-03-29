391ca609a1961d85585dbaaa02af8cc054f236ea
Tiago de Oliveira <74031973+deolivtiago@users.noreply.github.com>
2023-03-28 23:48:33 -0300

    v0.1.0 (#25) (#61)
    * feat: init phoenix app (#13)
    
    * build: include phoenix
    
    * docs: update readme
    
    * feat: setup database (#14)
    
    * chore: set uuid and timestamptz as default
    
    * feat: add base schema
    
    * chore: update database config
    
    * chore: add environment variables file
    
    * docs: update readme
    
    * chore: upgrade dependencies (#15)
    
    * chore: upgrade dependencies
    
    * chore: setup ci (#16)
    
    * chore: include credo
    
    * chore: setup code analyzer
    
    * chore: include excoveralls
    
    * chore: setup code coverage
    
    * chore: add alias
    
    * chore: setup ci
    
    * docs: add code status badges
    
    * chore: add support libraries (#17)
    
    * chore: include argon2
    
    * chore: include tzdata
    
    * chore: setup tzdata
    
    * chore: include ex_machina
    
    * chore: include faker
    
    * chore: setup ex_machina and faker
    
    * feat: add migration to create users table (#18)
    
    * feat: add migration to create users table
    
    * feat: add user validation schema (#19)
    
    * feat: add user validation schema
    
    * test: add user factory for testing
    
    * test: ensure schema validates user data
    
    * feat: add user management context (#20)
    
    * feat: add user listing
    
    * feat: update schema to create invalid changeset
    
    * feat: add user recovery
    
    * feat: add user creation
    
    * feat: add user changes
    
    * feat: add user exclusion
    
    * feat: add user management context
    
    * test: ensure user management handles users
    
    * feat: add routes to handle users (#21)
    
    * refactor: add success flag to the response
    
    * test: update view test to validate success flag
    
    * feat: add changeset response view
    
    * feat: add user response view
    
    * test: ensure changeset view renders changeset errors
    
    * test: ensure user view renders user data
    
    * feat: add fallback controller to handle errors
    
    * feat: add user controller to handle users
    
    * feat: add routes to handle users
    
    * test: ensure user controller handles users
    
    * feat: add auth to routes (#22)
    
    * chore: include guardian
    
    * chore: setup guardian
    
    * feat: add user recovery by email
    
    * test: ensure user management get user by email
    
    * feat: implement guardian modules
    
    * test: add tests to guardian modules
    
    * test: add user factory for testing
    
    * feat: add context to handle auth
    
    * test: ensure context handles auth
    
    * feat: add auth to user routes
    
    * feat: add auth view
    
    * test: ensure auth view renders auth data
    
    * test: update user controller tests
    
    * feat: add auth controller
    
    * test: ensure auth controller handles auth requests
    
    * chore: setup cd (#24)
    
    * chore: generate phoenix release
    
    * chore: setup cd

---
+++ lib/api/user_management/get.ex
@@ -0,0 +1,29 @@
+defmodule Api.UserManagement.Get do
+  @moduledoc false
+  alias Api.Repo
+  alias Api.UserManagement.User
+
+  @doc false
+  def call(email: email) do
+    User
+    |> Repo.get_by!(email: String.downcase(email))
+    |> then(&{:ok, &1})
+  rescue
+    _ ->
+      :email
+      |> User.invalid_changeset(email, "not found")
+      |> then(&{:error, &1})
+  end
+
+  @doc false
+  def call(id) do
+    User
+    |> Repo.get!(id)
+    |> then(&{:ok, &1})
+  rescue
+    _ ->
+      :id
+      |> User.invalid_changeset(id, "not found")
+      |> then(&{:error, &1})
+  end
+end
